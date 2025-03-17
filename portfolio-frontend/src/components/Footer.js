b2177586dbdc425075ffb5c620724686123e1aaa
shashank pandey <shashankpandey2001@gmail.com>
2025-03-18 00:05:54 +0530

    Update 1.0.5

--- portfolio-frontend/src/components/Footer.js
+++ portfolio-frontend/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { SiLeetcode } from 'react-icons/si';
 
 const Footer = () => {
   return (
@@ -9,6 +10,8 @@ const Footer = () => {
         <a href="https://github.com/shashank23-10" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
         <a href="https://www.linkedin.com/in/shashank2310/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
         <a href="mailto:shashankpandey2001@gmail.com"><FaEnvelope /></a>
+        <a href="https://leetcode.com/u/Dc0dER/" target="_blank" rel="noopener noreferrer"><SiLeetcode />
+                    </a>
       </div>
       <p>© {new Date().getFullYear()} Shashank Pandey. All rights reserved.</p>
     </footer>
